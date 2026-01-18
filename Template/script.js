6919cbcc10b88a44d0663610ef07e66f6c3a238d
hannakalvik <hanna.kalvik@gmail.com>
2026-01-18 20:46:58 +0100

    added the function and logic for the first gesture

--- Template/script.js
+++ Template/script.js
@@ -31,4 +31,31 @@ function setup() {
   window.requestAnimationFrame(loop);
 }
 
+// Event handler
+function onKey(event) {
+  const key = event.key.toLowerCase(); //event.key=which key .toLowerCase()=makes all the keys the same no matter if they are caps or not
+  state.lastKey = key;
+  state.pressed.push(key); //puts the newest value last in the array
+  console.log(state.pressed); //able to see it in dev tools
+}
+
+// Logic for the keyboard inputs 
+function checkSwipe() { //check if the array is correct 
+  let correct: true;
+  for (let i = 0; i < state.pressed.legth; i++) {
+    if (state.pressed[i] !== settings.sequence[i]) { // [i] = "hämta plats nummer i"
+      correct = false; //when it finds some key that isn't in the array it turns true into false
+   }
+  }
+
+  if (!correct) { 
+    state.pressed = []; //if it's false, empty the array and start over
+  }
+
+  if (state.pressed.lenght === settings.sequence.lenght) {
+    console.log("Gest Complete");
+    state.pressed = []; // if everything is correct in the array, the gesture is done
+  }
+}
+
 setup(); // Always remember to call setup()!
