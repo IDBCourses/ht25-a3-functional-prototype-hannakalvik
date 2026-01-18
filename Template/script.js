dba5aabf39d550880a9f8a9061aba5be9d96d3a2
hannakalvik <hanna.kalvik@gmail.com>
2026-01-18 21:50:43 +0100

    fixed some mistakes and added A-L gesture with progress, log, and SPACE button feedback

--- Template/script.js
+++ Template/script.js
@@ -14,22 +14,19 @@ const settings = {
 //State
 const state = {
   pressed: [], // Array that saves the key input
-  lastKey:"" 
+  lastKey:"" ,
   progress: 0 //How many times the gesture is correct
 };
 
 
 // Code that runs over and over again
 function loop() {
-
   window.requestAnimationFrame(loop);
 }
 
 // Setup is run once, at the start of the program. It sets everything up for us!
 function setup() {
-
   document.addEventListener("keydown", onKey);
-
   window.requestAnimationFrame(loop);
 }
 
@@ -38,9 +35,10 @@ function onKey(event) {
   const key = event.key.toLowerCase(); //event.key=which key .toLowerCase()=makes all the keys the same no matter if they are caps or not
   state.lastKey = key;
   state.pressed.push(key); //puts the newest value last in the array
-  console.log(state.pressed); //able to see it in dev tools
 
-  checkSwipe(); // logic
+  document.getElementById("logg").innerHTML = state.pressed.join(", "); //update the log immediatly 
+
+  checkSwipe(); // logic - check the gesture
   if (key === " ") {
     tryOpen(); // simple button interaction
   }
@@ -49,6 +47,7 @@ function onKey(event) {
 // Logic for the keyboard inputs 
 function checkSwipe() { //check if the array is correct 
   let correct = true;
+
   for (let i = 0; i < state.pressed.length; i++) {
     if (state.pressed[i] !== settings.sequence[i]) { // [i] = "hämta plats nummer i"
       correct = false; //when it finds some key that isn't in the array it turns true into false
@@ -57,29 +56,38 @@ function checkSwipe() { //check if the array is correct
 
   if (!correct) { 
     state.pressed = []; //if it's false, empty the array and start over
-    document.getElementById("status").innerHTML = "Wrong key! Start over.";
-  } else {
+    const statusEl = document.getElementById("Status")
+    statusEl.innerHTML = "Wrong key! Start over.";
+    statusEl.style.color = "red";
+  } else if (state.pressed.length < settings.sequence.length) {
     document.getElementById("status").innerHTML = "Swipe in progress...";
+    document.getElementById("status").style.color = "black";
   }
 
-  if (state.pressed.lenght === settings.sequence.length) {
+  // if gesture is complete
+  if (state.pressed.length === settings.sequence.length) {
     state.progress++;
     document.getElementById("status").innerHTML = "Gesture complete!";
     document.getElementById("progress").innerHTML =
         "Progress: " + state.progress + " / " + settings.needed;
-    state.pressed = []; // if everything is correct in the array, the gesture is done
+    state.pressed = []; // if everything is correct in the array, the gesture is done, reset for the next swipe
   }
-
-  document.getElementById("logg").innerHTML = state.pressed.join(", ");
 }
 
+//Open door function
 function tryOpen() {
+  const statusEl = document.getElementById("Status");
+  const progressEl = document.getElementById("Progress");
+
   if (state.progress >= settings.needed) {
-    console.log("Door open! You escaped!");
+    statusEl.innerHTML = "Door open! You escaped!";
+    statusEl.style.color = "green";
     state.progress = 0;
+    progressEl.innerHTML = "Progress: " + state.progress + " / " + settings.needed;
   } else {
-    console.log ("Not enough charge yet. Keep swiping!");
+    statusEl.innerHTML = "Not enough charge yet. Keep swiping!";
+    statusEl.style.color = "red";
   }
-  }
+ }
 
 setup(); // Always remember to call setup()!
